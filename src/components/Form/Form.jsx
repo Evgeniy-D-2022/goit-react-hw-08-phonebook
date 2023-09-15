061eb9ab0d986a30310c2380185b5f3edbe65b23
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-09-15 19:09:57 +0200

    add new patterns

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -35,7 +35,7 @@ const Form = () => {
         className={css.form__input}
         type="text"
         name="name"
-        pattern='^[a-zA-Zа-яА-ЯёЁ][a-zA-Z-а-яА-ЯёЁ ]+[a-zA-Zа-яА-ЯёЁ]?$'
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         placeholder="Name"
@@ -45,7 +45,7 @@ const Form = () => {
         className={css.form__input}
         type="tel"
         name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        pattern='\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}'
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
         placeholder="Number xxx-xx-xx"
